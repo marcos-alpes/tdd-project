338bb3eca41435f954e9399351d226cfc1c5a67c
Marcos Vinicius <mvap.marcos@gmail.com>
2026-04-21 19:45:05 -0300

    Refatorando o Teste Funcional

--- functional_tests.py
+++ functional_tests.py
@@ -17,24 +17,29 @@ class NewVisitorTest(unittest.TestCase):
         rows = table.find_elements(By.TAG_NAME, 'tr')
         self.assertIn(row_text, [row.text for row in rows])
 
-    def test_can_start_a_list_and_retrieve_it_later(self): 
-    
+    def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
 
         self.assertIn('To-Do', self.browser.title)
 
-        header_text = self.browser.find_element(By.TAG_NAME, 'h1').text  
+        header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
         self.assertIn('To-Do', header_text)
 
-        inputbox = self.browser.find_element(By.ID, 'id_new_item')  
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
         self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
         inputbox.send_keys('Estudar testes funcionais')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-        
         self.check_for_row_in_list_table('1: Estudar testes funcionais')
 
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox.send_keys('Estudar testes de unidade')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        self.check_for_row_in_list_table('1: Estudar testes funcionais')
+        self.check_for_row_in_list_table('2: Estudar testes de unidade')
 
 if __name__ == '__main__':
     unittest.main()
